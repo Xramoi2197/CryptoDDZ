4889dc47b2e117db0e0118f3ef0002abef7dba87
Xramoi <gitarist2197@gmail.com>
2018-10-19 23:15:45 +0300

    Закончил свою часть (Саня)

---
+++ CryptoDDZ/Algorythms.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CryptoDDZ
+{
+    class Algorythms
+    {
+        private Algorythm _algorythm;
+        public event Action<string> WriteResult;
+
+        public Algorythms(Action<string> writeAction)
+        {
+            WriteResult += writeAction;
+        }
+
+        public void DoAlg(string algName, Dictionary<string,string> parameters) //Функция выбирает нужный алгоритм и запускает его
+        {
+            switch (algName)//Сюда необходимо добавить свой алгоритм
+            {
+                case "Example":
+                {
+                    _algorythm = new PresentAlg();
+                    if (_algorythm.Fill(parameters, WriteResult))
+                    {
+                        _algorythm.Do();
+                    }
+                    break;
+                }
+                case "Shennon":
+                {
+                    _algorythm = new ShennonAlgorithm();
+                    if (_algorythm.Fill(parameters, WriteResult))
+                    {
+                        _algorythm.Do();
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    public abstract class Algorythm //От этого класса наследуются алгоритмы
+    {
+        public abstract event Action<string> WriteResult; //Event для вывода на экран
+        public abstract bool Fill(Dictionary<string, string> parameters, Action<string> writeAction); //Функция заполнения и проверки всех параметров алгоритма
+        public abstract void Do(); //Функция выполняет алгоритм и вываодит его в TextBox
+    }
+
+    /*Пример алгоритма*/
+    public class PresentAlg : Algorythm
+    {
+        public override event Action<string> WriteResult;
+        private int _x;
+        private int _y;
+
+        public override bool Fill(Dictionary<string, string> parameters, Action<string> writeAction)
+        {
+            WriteResult += writeAction;
+            if (parameters.Count != 2)
+            {
+                return false;
+            }
+            string a;
+            parameters.TryGetValue("x", out a);
+            if (a == null)
+            {
+                WriteResult?.Invoke("Параметр x не задан!");
+                return false;
+            }
+            if (!int.TryParse(a, out _x))
+            {
+                WriteResult?.Invoke("Параметр x не удалось привести к int!");
+                return false;
+            }
+            parameters.TryGetValue("y", out a);
+            if (a == null)
+            {
+                WriteResult?.Invoke("Параметр y не задан!");
+                return false;
+            }
+            if (!int.TryParse(a, out _y))
+            {
+                WriteResult?.Invoke("Параметр y не удалось привести к int!");
+                return false;
+            }
+            return true;
+        }
+
+        public override void Do()
+        {
+            WriteResult?.Invoke("x=" + _x);
+            WriteResult?.Invoke("y=" + _y);
+            int rez = _x + _y;
+            WriteResult?.Invoke("x+y=" + rez);
+
+        }
+    }
+
+    /*Алгоритм Шеннона(Шаг младенца, шаг великана)*/
+    public class ShennonAlgorithm : Algorythm
+    {
+        public override event Action<string> WriteResult;
+        private int _p;
+        private int _a;
+        private int _b;
+        private int _m;
+        private int _k;
+
+        public static Int64 MyPow(int x, int y, int p)
+        {
+            Int64 result = x;
+            for (int i = 0; i < y - 1; i++)
+            {
+                result = result % p * x;
+            }
+            return result % p;
+        }
+
+        public override bool Fill(Dictionary<string, string> parameters, Action<string> writeAction)
+        {
+            WriteResult += writeAction;
+            if (parameters.Count != 5)
+            {
+                return false;
+            }
+            string helpStr;
+            parameters.TryGetValue("p", out helpStr);
+            if (helpStr == null)
+            {
+                WriteResult?.Invoke("Параметр p не задан!");
+                return false;
+            }
+            if (!int.TryParse(helpStr, out _p))
+            {
+                WriteResult?.Invoke("Параметр p не удалось привести к int!");
+                return false;
+            }
+            parameters.TryGetValue("a", out helpStr);
+            if (helpStr == null)
+            {
+                WriteResult?.Invoke("Параметр a не задан!");
+                return false;
+            }
+            if (!int.TryParse(helpStr, out _a))
+            {
+                WriteResult?.Invoke("Параметр a не удалось привести к int!");
+                return false;
+            }
+            parameters.TryGetValue("b", out helpStr);
+            if (helpStr == null)
+            {
+                WriteResult?.Invoke("Параметр b не задан!");
+                return false;
+            }
+            if (!int.TryParse(helpStr, out _b))
+            {
+                WriteResult?.Invoke("Параметр b не удалось привести к int!");
+                return false;
+            }
+            parameters.TryGetValue("m", out helpStr);
+            if (helpStr == null)
+            {
+                WriteResult?.Invoke("Параметр m не задан!");
+                return false;
+            }
+            if (!int.TryParse(helpStr, out _m))
+            {
+                WriteResult?.Invoke("Параметр m не удалось привести к int!");
+                return false;
+            }
+            parameters.TryGetValue("k", out helpStr);
+            if (helpStr == null)
+            {
+                WriteResult?.Invoke("Параметр k не задан!");
+                return false;
+            }
+            if (!int.TryParse(helpStr, out _k))
+            {
+                WriteResult?.Invoke("Параметр k не удалось привести к int!");
+                return false;
+            }
+            if (_m * _k < _p)
+            {
+                WriteResult?.Invoke("Произвдение m*k меньше p!");
+                return false;
+            }
+            return true;
+        }
+
+        public override void Do()
+        {
+            int I = 0;
+            // ReSharper disable once InconsistentNaming
+            int J = 1;
+            var mult = _k * _m;
+            WriteResult?.Invoke("Произведение m и k равно " + mult);
+            if (mult < _p)
+            {
+                return;
+            }
+            Dictionary<int, double> numer1 = new Dictionary<int, double>();
+            Dictionary<int, double> numer2 = new Dictionary<int, double>();
+            for (int i = 0; i < _m; i++)
+            {
+                var rez = (Math.Pow(_a, i) * _b) % _p;
+                numer1.Add(i, rez);
+            }
+            for (int i = 1; i < _k + 1; i++)
+            {
+                var rez = MyPow(_a, i * _m, _p);
+                numer2.Add(i, rez);
+            }
+            WriteResult?.Invoke("Первый ряд:");
+            string numerable = string.Empty;
+            for (int i = 0; i < _m; i++)
+            {
+                numerable += numer1[i] + " ";
+            }
+            WriteResult?.Invoke(numerable);
+            WriteResult?.Invoke("Второй ряд:");
+            numerable = string.Empty;
+            for (int i = 1; i < _k + 1; i++)
+            {
+                numerable += numer2[i] + " ";
+            }
+            WriteResult?.Invoke(numerable);
+            for (int i = 0; i < _m; i++)
+            {
+                for (int j = 1; j < _k + 1; j++)
+                {
+                    // ReSharper disable once CompareOfFloatsByEqualityOperator
+                    if (numer1[i] == numer2[j])
+                    {
+                        J = i;
+                        I = j;
+                    }
+                }
+            }
+            WriteResult?.Invoke("i=" + I);
+            WriteResult?.Invoke("j=" + J);
+            var x = I * _m - J;
+            WriteResult?.Invoke("x=i*m-j=" + x);
+        }
+    }
+}
--- CryptoDDZ/MainWindow.xaml.cs
+++ CryptoDDZ/MainWindow.xaml.cs
@@ -1,28 +1,158 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CryptoDDZ
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        public void WriteResults(string result)
+        {
+            RezultBox.Text += result;
+            RezultBox.Text += "\r\n";
+        }
+
+        readonly Algorythms _algorythms;
+        private string _info;
+        private Dictionary<string, string> _parameters;
+        private List<DataGridRowEx> _rowExs;
+
         public MainWindow()
         {
+            _algorythms = new Algorythms(WriteResults);
             InitializeComponent();
         }
+
+        private void DoButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            RezultBox.Text = string.Empty;
+            if (AlgCombo.SelectedItem == null)
+            {
+                return;
+            }
+            foreach (DataGridRowEx dataGridItem in DataGrid.Items)
+            {
+                if (dataGridItem.Value == string.Empty)
+                {
+                    WriteResults("Недостаточно аргументов");
+                    return;
+                }
+            }
+            var text = AlgCombo.SelectedItem as ComboBoxItem;
+            _algorythms.DoAlg(text?.Name, _parameters);
+        }
+
+        private void AlgCombo_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+            if (selectedItem == null)
+            {
+                return;
+            }
+            ChangeView(selectedItem.Name);
+        }
+
+        private void ChangeView(string name)
+        {
+            _parameters = new Dictionary<string, string>();
+            _rowExs = new List<DataGridRowEx>();
+            RezultBox.Text = string.Empty;
+            
+            switch (name) //Сюда необходимо добавть свой алгоритм!!!
+            {
+                case "Example":
+                {
+                    _info = "Example text info"; //Info надо будет положить в ресурсы!!!
+                    _parameters.Add("x", string.Empty);
+                    _parameters.Add("y", string.Empty);
+                    DataGridRowEx newRow = new DataGridRowEx(1, "x", string.Empty);
+                    _rowExs.Add(newRow);
+                    newRow = new DataGridRowEx(2, "y", string.Empty);
+                    _rowExs.Add(newRow);
+                    _rowExs.Sort();
+                    DataGrid.ItemsSource = _rowExs;
+                    break;
+                }
+                case "Shennon":
+                {
+                    _info = "Shennon info";
+                    _parameters.Add("p", string.Empty);
+                    _parameters.Add("a", string.Empty);
+                    _parameters.Add("b", string.Empty);
+                    _parameters.Add("m", string.Empty);
+                    _parameters.Add("k", string.Empty);
+                    
+                    DataGridRowEx newRow = new DataGridRowEx(1, "p", string.Empty);
+                    _rowExs.Add(newRow);
+                    newRow = new DataGridRowEx(2, "a", string.Empty);
+                    _rowExs.Add(newRow);
+                    newRow = new DataGridRowEx(3, "b", string.Empty);
+                    _rowExs.Add(newRow);
+                    newRow = new DataGridRowEx(4, "m", string.Empty);
+                    _rowExs.Add(newRow);
+                    newRow = new DataGridRowEx(5, "k", string.Empty);
+                    _rowExs.Add(newRow);
+                    _rowExs.Sort();
+                    DataGrid.ItemsSource = _rowExs;
+                    break;
+                }
+            }
+            InfoBox.Text = _info;
+        }
+
+        private void DataGrid_OnCellEditEnding(object sender, DataGridCellEditEndingEventArgs e)//Исправить баг с переключением мышью
+        {        
+            var grid = sender as DataGrid;
+            var item = grid?.CurrentItem as DataGridRowEx;
+            if (e.EditAction == DataGridEditAction.Cancel || item == null)
+            {
+                return;
+            }
+            var text = e.EditingElement as TextBox;
+            _parameters[item.Variable] = text?.Text;
+            int count = grid.Items.Count;
+            if (e.Row.GetIndex() == count - 1)
+            {
+                DoButton.Focus();
+            }
+        }
+    }
+
+    public class DataGridRowEx : IComparable
+    {
+        private readonly int _num;
+        public string Variable { get; set; }
+        public string Value { get; set; }
+
+        public DataGridRowEx(int num, string variable, string value)
+        {
+            _num = num;
+            Variable = variable;
+            Value = value;
+        }
+
+        public int CompareTo(object obj)
+        {
+            var sec = obj as DataGridRowEx;
+            if (sec == null)
+            {
+                return -1;
+            }
+            if (_num == sec._num)
+            {
+                return 0;
+            }
+            if (_num > sec._num)
+            {
+                return 1;
+            }
+            return -1;
+        }
+
     }
 }
