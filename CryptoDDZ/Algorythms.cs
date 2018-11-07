fce19489b7dcdf8bd6e4c08e68e084ec00700609
Xramoi <gitarist2197@gmail.com>
2018-11-07 21:57:13 +0300

    Пытаюсь перенести изменения из egor

--- CryptoDDZ/Algorythms.cs
+++ CryptoDDZ/Algorythms.cs
@@ -384,6 +384,11 @@ namespace CryptoDDZ
     /*Алгоритм RSA*/
     class RsaAlgorithm : Algorythm
     {
+        public char[] Characters = new char[] {  'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+            'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+            'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+            'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', '0' };
         public override event Action<string> WriteResult;
         // ReSharper disable once InconsistentNaming
         private long _N;
